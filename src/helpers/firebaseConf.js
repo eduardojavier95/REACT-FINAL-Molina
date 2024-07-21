759933021d13a6a376cb2dc62d15e42ab8d566ab
Eduardo <emolina@sorba.ai>
2024-07-20 20:12:01 -0400

    fix env

--- src/helpers/firebaseConf.js
+++ src/helpers/firebaseConf.js
@@ -9,4 +9,14 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_APPID
 };
 
+console.log('Firebase Config:', {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
+  });
+  
+
 export const app = initializeApp(firebaseConfig);
